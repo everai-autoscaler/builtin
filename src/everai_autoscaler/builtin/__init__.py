592cb8761dccc2d56ade8e703b85998a5042f3a4
mc-j2 <mc@expvent.com>
2024-06-21 19:12:36 +0800

    0.1.45 upgrade to everai-autoscaler-0.1.20

--- src/everai_autoscaler/builtin/__init__.py
+++ src/everai_autoscaler/builtin/__init__.py
@@ -5,7 +5,7 @@ from .free_worker_autoscaler import FreeWorkerAutoScaler
 from everai_autoscaler.builtin.factory import Factory, Singleton
 from everai_autoscaler.model import BuiltinAutoScaler
 
-__version__ = "0.1.44"
+__version__ = "0.1.45"
 
 T = typing.TypeVar('T', bound=BuiltinAutoScaler)
 
